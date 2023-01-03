495109a3ecbf617d318a48319ea5535bdac8f777
Willem Verbuyst <willemverbuyst@gmail.com>
2023-01-03 19:59:08 +0100

    Create generic search function

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,33 @@
 import './App.css';
+import { search } from './business/search';
 import { books } from './dummyData/books';
 import { persons } from './dummyData/persons';
 
 function App() {
+  const query = 'h';
   return (
     <>
       <h1>Persons</h1>
       <ul>
-        {persons.map((person) => (
-          <li key={person._id}>
-            {person.firstName} {person.surname}
-          </li>
-        ))}
+        {persons
+          .filter((person) =>
+            search(person, ['firstName', 'surname'], query, false)
+          )
+          .map((person) => (
+            <li key={person._id}>
+              {person.firstName} {person.surname}
+            </li>
+          ))}
       </ul>
       <h1>Books</h1>
       <ul>
-        {books.map((book) => (
-          <li key={book._id}>
-            {book.title} {book.pages}
-          </li>
-        ))}
+        {books
+          .filter((book) => search(book, ['title'], query, false))
+          .map((book) => (
+            <li key={book._id}>
+              {book.title} {book.pages}
+            </li>
+          ))}
       </ul>
     </>
   );
---
+++ src/business/search.ts
@@ -0,0 +1,23 @@
+export function search<T>(
+  object: T,
+  properties: Array<keyof T>,
+  query: string,
+  shouldBeCaseSensitive: boolean
+): boolean {
+  if (query === '') {
+    return true;
+  }
+
+  return properties.some((property) => {
+    const value = object[property];
+
+    if (typeof value === 'string' || typeof value === 'number') {
+      if (shouldBeCaseSensitive) {
+        return value.toString().includes(query);
+      } else {
+        return value.toString().toLowerCase().includes(query.toLowerCase());
+      }
+    }
+    return false;
+  });
+}
