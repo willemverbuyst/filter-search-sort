9edb15ddabb7d8b49fd4a8f6bdc5ea21bfdfcd00
Willem Verbuyst <willemverbuyst@gmail.com>
2023-01-03 19:43:39 +0100

    Create dummy data for books

---
+++ src/dummyData/books.ts
@@ -0,0 +1,74 @@
+import { Book } from '../interfaces/Book';
+
+export const books: Array<Book> = [
+  {
+    _id: 'd944ff69-d152-4aa6-a208-3c9e3b083482',
+    tile: 'Fugiat in incididunt excepteur laborum consectetur tempor minim magna veniam ad fugiat proident.',
+    author: 'Angelica Hyde',
+    pages: 312,
+    inPrint: true,
+  },
+  {
+    _id: '6420492c-2b29-4f2d-a90b-c4e59256182d',
+    tile: 'Et anim nisi proident irure esse Lorem officia.',
+    author: 'Shawn Lane',
+    pages: 87,
+    inPrint: true,
+  },
+  {
+    _id: 'f49f332e-f74e-4ee4-a8d4-46edce46d4b1',
+    tile: 'Elit dolor enim excepteur veniam duis labore in Lorem.',
+    author: 'Ramsey Lindsay',
+    pages: 112,
+    inPrint: false,
+  },
+  {
+    _id: '0aa03bf4-9b0f-4a59-ad0d-ea7719864cae',
+    tile: 'Ipsum officia mollit ullamco laborum proident enim eiusmod consectetur enim.',
+    author: 'Alejandra Lott',
+    pages: 63,
+    inPrint: false,
+  },
+  {
+    _id: '0ce92757-b7c4-43c0-9a1b-ce54164248f3',
+    tile: 'Ipsum reprehenderit reprehenderit aliquip labore aliquip excepteur labore.',
+    author: 'Odom Bentley',
+    pages: 86,
+    inPrint: false,
+  },
+  {
+    _id: 'd7682d0e-a8b1-4cbf-a100-403c5c3b3375',
+    tile: 'Incididunt cillum occaecat sint esse esse sint dolor enim dolore sit.',
+    author: 'Tommie Walters',
+    pages: 256,
+    inPrint: false,
+  },
+  {
+    _id: '5a5a7a51-de75-4c84-b265-d07b4707c361',
+    tile: 'Fugiat proident incididunt magna velit nulla duis nostrud anim.',
+    author: 'Jimenez Hunt',
+    pages: 319,
+    inPrint: true,
+  },
+  {
+    _id: 'ede0a89b-abb9-4b14-8df1-8107358f8457',
+    tile: 'Et laborum excepteur tempor ullamco proident elit.',
+    author: 'Margo Shepard',
+    pages: 359,
+    inPrint: false,
+  },
+  {
+    _id: '8b4e69a0-cf67-414a-9bdf-ec8a87126028',
+    tile: 'Ipsum et in sint dolor nostrud irure eu reprehenderit consequat et.',
+    author: 'Janice Santos',
+    pages: 155,
+    inPrint: false,
+  },
+  {
+    _id: '722d029d-323d-473c-ad8b-170a6a1c09ea',
+    tile: 'Fugiat cillum nostrud elit duis dolor Lorem adipisicing irure.',
+    author: 'Cain Walls',
+    pages: 174,
+    inPrint: true,
+  },
+];
