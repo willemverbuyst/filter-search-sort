35b24940de27c65dd6551d9f1c9fc17cce2e3373
Willem Verbuyst <willemverbuyst@gmail.com>
2023-01-03 19:36:25 +0100

    Create Person interface

---
+++ src/interfaces/Person.ts
@@ -0,0 +1,12 @@
+export interface Person {
+  _id: string;
+  firstName: string;
+  surname: string;
+  age: number;
+  eyeColor: string;
+  gender: 'male' | 'female';
+  company: string;
+  email: string;
+  married: boolean;
+  birtDate: Date;
+}
