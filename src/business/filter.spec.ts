c06b8d8e71e7d184c3cef1f88b6c37982f857461
Willem Verbuyst <willemverbuyst@gmail.com>
2023-01-11 05:59:00 +0100

    Write test for filter

--- src/business/filter.spec.ts
+++ src/business/filter.spec.ts
@@ -1,5 +1,28 @@
 import { filter } from "./filter";
 
-test("True is truthy", () => {
-  expect(true).toBeTruthy();
+describe("Filter", () => {
+  it("should return true when filter properties is an empty array", () => {
+    expect(filter({ foo: 1 }, [])).toBe(true);
+  });
+
+  it("should return true if truthty flag is true", () => {
+    expect(
+      filter({ foo: 1 }, [{ property: "foo", isTruthySelected: true }])
+    ).toBe(true);
+    expect(
+      filter({ foo: true }, [{ property: "foo", isTruthySelected: true }])
+    ).toBe(true);
+  });
+
+  it("should return false if thruthy flag is false", () => {
+    expect(
+      filter({ foo: 1 }, [{ property: "foo", isTruthySelected: false }])
+    ).toBe(false);
+  });
+
+  it("should return true if truthty flag is false and prop is false", () => {
+    expect(
+      filter({ foo: false }, [{ property: "foo", isTruthySelected: false }])
+    ).toBe(true);
+  });
 });
