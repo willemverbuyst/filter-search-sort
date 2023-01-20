e1160fb757293cd6a3a3925b008310a96c11f1b8
Willem Verbuyst <willemverbuyst@gmail.com>
2023-01-20 04:53:38 +0100

    test: make search tests more readable

--- src/business/search.spec.ts
+++ src/business/search.spec.ts
@@ -1,37 +1,76 @@
 import { genericSearch } from "./search";
 
-const testObject1 = {
-  foo: "X_test",
-  bar: "Z_test",
-  quuz: 1,
-  quux: true,
-};
+describe("genericSearch", () => {
+  test("returns true when query is found in object properties", () => {
+    const object = { name: "John", age: 30, email: "doe@example.com" };
+    const properties: Array<keyof typeof object> = ["name", "email"];
+    const query = "john";
+    const shouldBeCaseSensitive = false;
 
-describe("genericSort", () => {
-  it("should return true", () => {
-    expect(genericSearch(testObject1, ["foo", "bar"], "", true)).toEqual(true);
-    expect(genericSearch(testObject1, ["foo", "bar"], "X", true)).toEqual(true);
-    expect(genericSearch(testObject1, ["foo", "bar"], "x", false)).toEqual(
-      true
-    );
-    expect(genericSearch(testObject1, ["foo", "bar"], "x")).toEqual(true);
     expect(
-      genericSearch(testObject1, ["foo", "bar", "quuz"], "1", true)
-    ).toEqual(true);
+      genericSearch(object, properties, query, shouldBeCaseSensitive)
+    ).toBe(true);
   });
 
-  it("should return false", () => {
-    expect(genericSearch(testObject1, ["foo", "bar"], "x", true)).toEqual(
-      false
-    );
-    expect(genericSearch(testObject1, ["foo", "bar"], "A", false)).toEqual(
-      false
-    );
+  test("returns false when query is not found in object properties", () => {
+    const object = { name: "John", age: 30, email: "doe@example.com" };
+    const properties: Array<keyof typeof object> = ["name", "email"];
+    const query = "Jane";
+    const shouldBeCaseSensitive = false;
+
     expect(
-      genericSearch(testObject1, ["foo", "bar", "quuz"], "2", true)
-    ).toEqual(false);
+      genericSearch(object, properties, query, shouldBeCaseSensitive)
+    ).toBe(false);
+  });
+
+  test("handles case sensitivity correctly", () => {
+    const object = { name: "John", age: 30, email: "doe@example.com" };
+    const properties: Array<keyof typeof object> = ["name", "age"];
+    const query = "john";
+    const shouldBeCaseSensitiveTrue = true;
+    const shouldBeCaseSensitiveFalse = false;
+
     expect(
-      genericSearch(testObject1, ["foo", "bar", "quux"], "2", true)
-    ).toEqual(false);
+      genericSearch(object, properties, query, shouldBeCaseSensitiveTrue)
+    ).toBe(false);
+    expect(
+      genericSearch(object, properties, query, shouldBeCaseSensitiveFalse)
+    ).toBe(true);
+    expect(genericSearch(object, properties, query)).toBe(true);
+  });
+
+  test("handles case search for numeric value", () => {
+    const object = { name: "John", age: 30, email: "doe@example.com" };
+    const properties: Array<keyof typeof object> = ["name", "age"];
+    const query = "30";
+    const shouldBeCaseSensitive = true;
+
+    expect(
+      genericSearch(object, properties, query, shouldBeCaseSensitive)
+    ).toBe(true);
+  });
+
+  test("handles empty query correctly", () => {
+    const object = { name: "John", age: 30, email: "doe@example.com" };
+    const properties: Array<keyof typeof object> = ["name", "email"];
+    const query = "";
+    const shouldBeCaseSensitive = true;
+
+    expect(
+      genericSearch(object, properties, query, shouldBeCaseSensitive)
+    ).toBe(true);
+  });
+
+  test("handles case when value of property is not a string or number", () => {
+    const object = {
+      name: "John",
+      age: 30,
+      email: "doe@example.com",
+      tested: false,
+    };
+    const properties: Array<keyof typeof object> = ["name", "email", "tested"];
+    const query = "true";
+
+    expect(genericSearch(object, properties, query)).toBe(false);
   });
 });
