f4742caaac530fcd29124ffeec7cdfd1640c80e9
Willem Verbuyst <willemverbuyst@gmail.com>
2023-01-21 10:43:22 +0100

    test: update filter tests

--- src/business/filter.spec.ts
+++ src/business/filter.spec.ts
@@ -1,24 +1,28 @@
 import { genericFilter } from "./filter";
 
-const testObject = {
-  foo: 1,
-  bar: "test",
-  quux: true,
-  quuz: false,
-};
-
 describe("genericFilter", () => {
   it("should return true when filter properties is an empty array", () => {
+    const testObject = {
+      foo: 1,
+      bar: "test",
+      quux: true,
+      quuz: false,
+    };
+
     expect(genericFilter(testObject, [])).toBe(true);
   });
 
-  it("should return true", () => {
-    expect(
-      genericFilter(testObject, [{ property: "foo", isTruthySelected: true }])
-    ).toBe(true);
+  it("should return true when all filter properties match", () => {
+    const testObject = {
+      foo: 1,
+      bar: "test",
+      quux: true,
+      quuz: false,
+    };
+
     expect(
       genericFilter(testObject, [
-        { property: "bar", isTruthySelected: true },
+        { property: "foo", isTruthySelected: true },
         { property: "bar", isTruthySelected: true },
         { property: "quux", isTruthySelected: true },
         { property: "quuz", isTruthySelected: false },
@@ -26,13 +30,17 @@ describe("genericFilter", () => {
     ).toBe(true);
   });
 
-  it("should return false", () => {
-    expect(
-      genericFilter(testObject, [{ property: "quuz", isTruthySelected: true }])
-    ).toBe(false);
+  it("should return false when one filter property does not match", () => {
+    const testObject = {
+      foo: 1,
+      bar: "test",
+      quux: true,
+      quuz: false,
+    };
+
     expect(
       genericFilter(testObject, [
-        { property: "bar", isTruthySelected: true },
+        { property: "foo", isTruthySelected: true },
         { property: "bar", isTruthySelected: true },
         { property: "quux", isTruthySelected: true },
         { property: "quuz", isTruthySelected: true },
