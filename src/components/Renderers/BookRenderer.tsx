b1e82b1eac02f4ed7c8b9ec352cbb3196dacf176
Willem Verbuyst <willemverbuyst@gmail.com>
2025-01-02 09:25:29 +0100

    Use card component from shadcn

--- src/components/Renderers/BookRenderer.tsx
+++ src/components/Renderers/BookRenderer.tsx
@@ -1,26 +1,29 @@
-import { Card } from "react-bootstrap";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import Moment from "react-moment";
-
 import { Book } from "../../interfaces/Book";
-import styles from "../../styles/card.module.css";
 
 export function BookRenderer(props: Book): JSX.Element {
   const { _id, author, createdAt, pages, inPrint, title } = props;
 
   return (
-    <Card
-      style={{ width: "30vw" }}
-      className={inPrint ? `${styles.specialCard} m-2` : "m-2"}
-    >
-      <Card.Header>{author}</Card.Header>
-      <Card.Body>
-        <Card.Title>{title}</Card.Title>
-        <Card.Text>pages: {pages} </Card.Text>
-        <Card.Text>
+    <Card>
+      <CardHeader>
+        <CardTitle>{title}</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <p>author: {author}</p>
+        <p>pages: {pages} </p>
+        <p>
           createdAt: <Moment date={createdAt} format="YYYY/MM/DD" />
-        </Card.Text>
-      </Card.Body>
-      <Card.Footer>#{_id}</Card.Footer>
+        </p>
+      </CardContent>
+      <CardFooter>id: {_id}</CardFooter>
     </Card>
   );
 }
--- src/components/Renderers/PeopleRenderer.tsx
+++ src/components/Renderers/PeopleRenderer.tsx
@@ -1,24 +1,28 @@
-import { Card } from "react-bootstrap";
-
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { Person } from "../../interfaces/Person";
-import styles from "../../styles/card.module.css";
 
 export function PeopleRenderer(props: Person): JSX.Element {
   const { _id, firstName, age, eyeColor, surname, email } = props;
   return (
-    <Card
-      style={{ width: "30vw" }}
-      className={eyeColor === "blue" ? `${styles.specialCard} m-2` : "m-2"}
-    >
-      <Card.Header>
-        {firstName} {surname}
-      </Card.Header>
-      <Card.Body>
-        <Card.Title>{email}</Card.Title>
-        <Card.Text>{eyeColor}</Card.Text>
-        <Card.Text>{age}</Card.Text>
-      </Card.Body>
-      <Card.Footer>#{_id}</Card.Footer>
+    <Card>
+      <CardHeader>
+        <CardTitle>
+          {firstName} {surname}
+        </CardTitle>
+      </CardHeader>
+
+      <CardContent>
+        <p>email: {email}</p>
+        <p>eye color: {eyeColor}</p>
+        <p>age: {age}</p>
+      </CardContent>
+      <CardFooter>id: {_id}</CardFooter>
     </Card>
   );
 }
